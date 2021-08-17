3471d96d833e88449831fa3620362e43e333c0da
gantz27 <bielguedes484@gmail.com>
2021-08-17 11:48:52 -0300

    aula sobre typescript finalizada

--- index.ts
+++ index.ts
@@ -1,1 +1,83 @@
-console.log('TypeScript');
+// types >>> usado para fazer junções / merges entre interfaces
+
+// interfaces >>> usado para definir contrato de estruturas de dados/classes
+
+/* Generic types
+
+function adicionaApendiceALista<T>(array: T[], valor: T) {
+    return array.map(() =>  valor);
+}
+
+adicionaApendiceALista([1, 2, 3], 5)
+*/
+
+
+/*
+interface IUsuario {
+    id: string
+    email: string
+
+}
+
+interface IAdmin extends IUsuario {
+    cargo: 'gerente' | 'coordenador' | 'supervisor';
+}
+
+function redirecione(usuario: IUsuario | IAdmin) {
+    if ('cargo' in usuario) {
+        // redirecionar para a área de admin
+    }
+
+    // redirecionar para a área do usuário comum
+}*/
+
+/*
+interface IUsuario {
+    id: string
+    email: string
+    cargo?: 'gerente' | 'coordenador' | 'supervisor' | 'funcionario';
+}
+
+function redirecione(usuario: IUsuario) {
+    if (usuario.cargo) {
+        // redirecionar(usuario.cargo)
+    }
+
+    // redirecionar para a área do usuário
+}*/
+
+
+/*
+interface Cachorro {
+    nome: string;
+    idade: number;
+    parqueFavorito?: string;
+}
+
+type CachorroSomenteLeitura = {
+    readonly [K in keyof Cachorro]-?: Cachorro[K];
+}
+
+class MeuCachorro implements CachorroSomenteLeitura {
+    idade;
+    nome;
+    parqueFavorito;
+
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
+
+const cao = new MeuCachorro('Apolo', 14);
+*/
+
+import $ from 'jquery'
+
+$.fn.extend({
+    novaFuncao() {
+        console.log('Chamou nova funcao');
+    }
+});
+
+(<any>$('body')).novaFuncao();
---
+++ typings.d.ts
@@ -0,0 +1,3 @@
+interface jQuery {
+    novaFuncao(): void;
+}
